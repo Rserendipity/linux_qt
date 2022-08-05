9cf627a0d8cf39a4eb8a803cdea4a10477e91610
chang-jing-jing <2249077862@qq.com>
2022-08-05 11:45:02 +0800

    first use qt

---
+++ 22_8_5/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void test(){
+    std::thread th1;
+    for (int i = 0; i < 10; i++) {
+
+    }
+}
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+
